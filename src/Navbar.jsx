6505bd097e6a8d41864e4f946bc15af3fba4d8d5
Shriharsh <shriharshlul@gmail.com>
2022-12-22 17:01:37 +0545

    a boring day

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -2,8 +2,13 @@ import React from "react"
 import { NavLink } from "react-router-dom";
 const Navbar = ()=>{
    return(
+<>
+    <div className="container-fluid nav_bg">
+      <div className="row">
+        <div className="col-10 ms-auto ">
+
       <nav className="navbar sticky-top navbar-expand-lg transparent" id="nav1">
-      <Navliink className="brand-name" style={{color:"white"}}to="/">Shriharsh's Colosseum</Navliink>
+      <NavLink className="brand-name" style={{color:"white"}} to="/">Shriharsh's Colosseum</NavLink>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
@@ -11,17 +16,22 @@ const Navbar = ()=>{
       <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav ms-auto brand-desc text-uppercase">
           <li className="nav-item active">
-            <Navliink className="nav-link " style={{color:"white"}} to="./">Home</Navliink>
+            <NavLink className="nav-link " style={{color:"white"}} to="/">Home</NavLink>
           </li>
           <li className="nav-item">
-            <Navliink className="nav-link"  style={{color:"white"}} to="./projects">Projects</Navliink>
+            <NavLink className="nav-link"  style={{color:"white"}} to="/projects">Projects</NavLink>
           </li>
           <li className="nav-item">
-            <Navliink className="nav-link" style={{color:"white"}} to="./contact">Contact</Navliink>
+            <NavLink className="nav-link" style={{color:"white"}} to="/contact">Contact</NavLink>
    </li>
         </ul>
       </div>
     </nav>
+
+        </div>
+      </div>
+    </div>
+   </>
    );
 };
 
