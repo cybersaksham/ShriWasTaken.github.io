69a6d737e3170f349082cfe62dc872a83c6ba967
Saksham Bindal <saksham.bindal2004@gmail.com>
2023-01-03 18:34:53 +0530

    bootstrap fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
-import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import "./../node_modules/bootstrap/dist/js/bootstrap.bundle";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle";
 import React from "react";
 import Projects from "./Projects";
 import Contact from "./Contact";
-import {Route} from "react-router-dom";
+import { Route } from "react-router-dom";
 import Home from "./Home";
 import Navbar from "./Navbar";
-import {Routes,BrowserRouter as Router} from "react-router-dom";
-
+import { Routes, BrowserRouter as Router } from "react-router-dom";
 
 const App = () => {
-    return(
-        <>
-            <style>{'body { background-color: black; }'}</style>                
-                <Router>
-                <Navbar/>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route exact path="/contact/" element = {<Contact/>}/>
-                        <Route exact path="/projects/" element = { <Projects/>}/>
-                    </Routes>
-                </Router>
-        </>
-    );
-
+  return (
+    <>
+      <style>{"body { background-color: black; }"}</style>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route exact path="/contact" element={<Contact />} />
+          <Route exact path="/projects" element={<Projects />} />
+        </Routes>
+      </Router>
+    </>
+  );
 };
 export default App;
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,20 +1,36 @@
-    import React from "react";
-    import "./../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-    function Home(){
-        return(
-            <>
-            <div className="brand-desc">            
-                <p className="text-left mx-5 my-1" style={{fontSize:"100px",color:"white"}}>Shriharsh Acharya,
-                </p><br/>
-                <p>
-                    <a  style={{fontSize:"50px",textDecoration:"none" ,color:"white"}} className="text-left mx-5 my-1">an ambitious technophile</a>
-                </p><p>
-                    <a className="text-left my-1 mx-5" style={{fontSize:"30px",color:"white",textDecoration:"none"}}>learn more.</a></p>
-            </div>
-            </>
+function Home() {
+  return (
+    <>
+      <div className="brand-desc">
+        <p
+          className="text-left mx-5 my-1"
+          style={{ fontSize: "100px", color: "white" }}
+        >
+          Shriharsh Acharya,
+        </p>
+        <br />
+        <p>
+          <a
+            style={{ fontSize: "50px", textDecoration: "none", color: "white" }}
+            className="text-left mx-5 my-1"
+          >
+            an ambitious technophile
+          </a>
+        </p>
+        <p>
+          <a
+            className="text-left my-1 mx-5"
+            style={{ fontSize: "30px", color: "white", textDecoration: "none" }}
+          >
+            learn more.
+          </a>
+        </p>
+      </div>
+    </>
+  );
+}
 
-        );
-    };
-
-    export default Home;
+export default Home;
